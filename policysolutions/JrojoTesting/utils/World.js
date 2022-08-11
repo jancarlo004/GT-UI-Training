ef1bc03f8fd5ae74e1b9cac87c01a4d940916c78
Jennedie Rojo <jennedie.rojo@norimaconsulting.com>
2022-08-11 14:04:12 +0800

    Added World

---
+++ policysolutions/JrojoTesting/utils/World.js
@@ -0,0 +1,50 @@
+ module.exports = {
+    //for input of contact
+    // contact: {
+    //     firstName: "Test",
+    //     lastName: "D2V5",
+    //     newFirstName:"Test",
+    //     newLastName:"New",
+    // },
+    //for input of address
+    address_generator:{
+
+        Address_Line1:"508 Newberry Dr",
+        Address_city: "Allen",
+        Address_county:"Collin",
+        Address_state:"TX",
+        Address_zipcode:"75013",
+        Address_type:"home",
+    },
+
+    //for input of Company 
+    company_generator:{
+
+        company_name:"Enigma",
+        company_producercode:"INT-3 Internal Producer Code - 3",
+        
+    },
+
+    //for personal auto new submission
+    lineofbussiness: ["Personal Auto","Homeowners","Inland Marine"],
+    offersauto:["Basic Program","Standard Program","Premium Program"],
+    qualificationmenu:["none","Yes","No - New Driver","No - previous policy did not renew","Not known"],
+    add_driver_menu:["New Person","From Address Book","Existing Driver"],
+
+    //Personal Informations   
+    DOB:"05/14/1990",
+    maritalstatus:["Divorced","Domestic partner","Married","Separated","Single","Unknown","Widowed"],
+    phone_Home:"325-672-5897",
+    pEmail:"test@gmail.com",
+    //drivers_license:"675456773",
+    //new_drivers_license:"675456772",
+    drivers_state:"Texas",
+    license_year:"2005",
+    accidents_policy_level:"0",
+    account_level:"0",
+    VINNumber:["3C8FY68B82T297664","1GNFC13C77J435851","1C6RR6KT6ES419075"],
+    cost_New:"120",
+
+    driver:[]
+    
+};
