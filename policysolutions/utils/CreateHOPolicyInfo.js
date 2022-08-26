1b8015975666bc166d72164780858b49391e3f7d
ltagaminorima <106361378+ltagaminorima@users.noreply.github.com>
2022-08-26 09:39:56 +0800

    Create CreateHOPolicyInfo.js

---
+++ policysolutions/utils/CreateHOPolicyInfo.js
@@ -0,0 +1,12 @@
+module.exports = {
+    policyType: "Dwelling",
+    coverageForm: "HO2",
+    WhoOccupies: "Myself",
+    DoyouOccupythisDwelling: "Yes",
+    FireProtectionClass: "Superior",
+    Wiring: "Copper",
+    Coinsurance: "50%",   
+    LossOfRentalIncome: "Yes",
+    ProhibitedUseCivilAuthority: "45 Days"
+
+};
