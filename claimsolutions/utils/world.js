5234a125193d4ee368644a2b021895afe6698ee6
ltagaminorima <106361378+ltagaminorima@users.noreply.github.com>
2022-08-26 09:27:13 +0800

    Add files via upload

---
+++ claimsolutions/utils/world.js
@@ -0,0 +1,18 @@
+module.exports = {
+    arrType: ['Vehicle','Vehicle'],
+    arrCoverage: ['Liability - Bodily Injury and Property Damage', 'Collision'],
+    arrClaimant: ['Ray Newton','Ray Newton'],
+    arrAdjuster: ['Thomas Sanders','Thomas Sanders'],
+    arrStatus: ['Open','Open'],
+    arrRemaining_reserves: ['$3,000.00','$3,000.00'],
+    arrFuture_payments: ['-','-'],
+    arrPaid: ['-','-'],
+    arrclaimNumber: '',
+    arrinvoiceNumber: '',
+    addCoverageToVehicle: true,
+    arrServicesTobeSelected: ['Adjudicate claim', 'Glass'],
+    arrVehicleCoverageType: ['Collision','Comprehensive'],
+    arrVehiclCoverageExposureLimit: ['10000','10000'],
+    arrVehiclCoverageIncidentLimit: ['10000','10000'],
+    arrVehiclestoSelect: ['Audi','Buick']
+};
